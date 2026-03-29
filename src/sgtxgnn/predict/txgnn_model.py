cd0b2b702c4e13dc7fdb888314ace6627bb889f2
Light <lightman.chang@gmail.com>
2026-03-29 21:15:42 +0800

    Update Guide to PHARMACOLOGY DDI data to v2026.1

--- src/sgtxgnn/predict/txgnn_model.py
+++ src/sgtxgnn/predict/txgnn_model.py
@@ -57,7 +57,7 @@ def check_dependencies() -> Tuple[bool, List[str]]:
         missing.append("dgl")
 
     try:
-        import sgtxgnn
+        import txgnn
     except ImportError:
         missing.append("TxGNN")
 
